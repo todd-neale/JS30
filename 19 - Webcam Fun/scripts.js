a2942007d9fc5c1d09a59f250fe6bb95ae413c8c
Todd <toddneale13@gmail.com>
2022-01-23 12:00:46 +0000

    voice search woah

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,15 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {    
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })        
+    .then(localMediaStream => {            
+    console.log(localMediaStream);
+    video.srcObject = window.URL.createObjectURL(localMediaStream);            
+    video.play();        
+  }).catch(error => console.error(`OH NO!!! ` ,error));
+
+}
